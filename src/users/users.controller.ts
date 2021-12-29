7a7d8afb81a6908ec2a1ee81d89ee0a2865c33e6
jesusGZ <j.gr.zurita@gmail.com>
2021-12-29 11:34:34 -0600

    agregadio de JwtAuthGuard a rutas de usuarios

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   show(@Param('id') id: string) {
     return this.usersService.showById(+id);
